33552ff24488317336a72e88a26050fb24e82ec8
Jamaria Sims <75871993+JamariaSims@users.noreply.github.com>
2021-09-22 22:56:56 -0400

    Routes for update&get

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,20 +1,25 @@
-const jwt = require('jsonwebtoken')
-const { JWT_SECRET } = require('../../env-connect')
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../../env-connect");
 
-function restricted (req, res, next) {
-    const token = req.headers.authorization
-    if(token){
-        jwt.verify(token, JWT_SECRET, (err, decode) => {
-            if (err) {
-              next({ status: 401, message: "Token invalid" })
-            }else{
-              req.decodedJWT = decode;
-              next();
-            }
-        })
-    }else{
-        next({status: 401, message: "token required"})
-    }
+function restricted(req, res, next) {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, JWT_SECRET, (err, decode) => {
+      if (err) {
+        next({ status: 401, message: "Token invalid" });
+      } else {
+        req.decodedJWT = decode;
+        next();
+      }
+    });
+  } else {
+    next({ status: 401, message: "token required" });
+  }
+}
+function logger(req, res, next) {
+  console.log("METHOD: ", req.method);
+  console.log("REQUEST_BODY: ", req.body);
+  next();
 }
 
-module.exports = { restricted }
+module.exports = { restricted, logger };
--- api/users/users-middleware.js
+++ api/users/users-middleware.js
@@ -1,64 +1,74 @@
-const phone = require('libphonenumber')
-const schema = require('./userPayloadSchema')
-const { findBy } = require('./users-model')
+const phone = require("libphonenumber");
+const schema = require("./userPayloadSchema");
+const { findBy } = require("./users-model");
 
-async function validatePayload (req, res, next) {
-    try{
-        //If doing 10-digit US numbers only:
-        //req.body.phone = ['+1', req.body.phone].join('')
-        await schema.validate(req.body);
-        req.phone_number = await phone.e164(req.body.phone);
-        next()
-    } catch (err) {
-        if (err.errors){
-            next({ status: 400, message: err.errors[0] });
-        }else{
-            next({ status: 400, message: err });
-        }
+async function validatePayload(req, res, next) {
+  try {
+    //If doing 10-digit US numbers only:
+    //req.body.phone = ['+1', req.body.phone].join('')
+    await schema.validate(req.body);
+    req.phone_number = await phone.e164(req.body.phone);
+    console.log("validatePayload: passed");
+    next();
+  } catch (err) {
+    console.log("validatePayload: failed");
+    if (err.errors) {
+      next({ status: 400, message: err.errors[0] });
+    } else {
+      next({ status: 400, message: err });
     }
+  }
 }
 
-function validateLoginPayload (req, res, next) {
-    const { username, password } = req.body
-    if(username && password){
-        next()
-    }else{
-        next({status: 400, message: 'username and password are required'})
-    }
+function validateLoginPayload(req, res, next) {
+  const { username, password } = req.body;
+  if (username && password) {
+    console.log("validateLoginPayload: passed");
+    next();
+  } else {
+    console.log("validateLoginPayload: failed");
+    next({ status: 400, message: "username and password are required" });
+  }
 }
 
-async function checkUsernameAvailable (req, res, next) {
-    const [user] = await findBy({username: req.body.username});
-    if (user) {
-        next({ status: 409, message: 'username taken' })
-    }else{
-        next()
-    }
+async function checkUsernameAvailable(req, res, next) {
+  const [user] = await findBy({ username: req.body.username });
+  if (user) {
+    console.log("checkUsernameAvailable: passed");
+    next({ status: 409, message: "username taken" });
+  } else {
+    console.log("checkUsernameAvailable: failed");
+    next();
+  }
 }
 
-async function checkPhoneAvailable (req, res, next) {
-    const [user] = await findBy({phone_number: req.phone_number});
-    if (user) {
-        next({ status: 409, message: 'phone number taken' });
-    }else{
-        next();
-    }
+async function checkPhoneAvailable(req, res, next) {
+  const [user] = await findBy({ phone_number: req.phone_number });
+  if (user) {
+    console.log("checkPhoneAvailable: failed");
+    next({ status: 409, message: "phone number taken" });
+  } else {
+    console.log("checkPhoneAvailable: passed");
+    next();
+  }
 }
 
-async function checkUsernameExists (req, res, next) {
-    const [user] = await findBy({username: req.body.username})
-    if (user) {
-        req.user = user
-        next()
-    }else {
-        next({status: 401, message: 'username or password invalid'})
-    }
+async function checkUsernameExists(req, res, next) {
+  const [user] = await findBy({ username: req.body.username });
+  if (user) {
+    console.log("checkUsernameExists: passed");
+    req.user = user;
+    next();
+  } else {
+    console.log("checkUsernameExists: failed");
+    next({ status: 401, message: "username or password invalid" });
+  }
 }
 
 module.exports = {
-    validatePayload,
-    checkUsernameAvailable,
-    checkPhoneAvailable,
-    validateLoginPayload,
-    checkUsernameExists
-}
+  validatePayload,
+  checkUsernameAvailable,
+  checkPhoneAvailable,
+  validateLoginPayload,
+  checkUsernameExists,
+};
